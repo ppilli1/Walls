c078efb82f0ae029ca5c6ce1fed441736273ccb4
starninjaz8055 <67479423+starninjaz8055@users.noreply.github.com>
2020-07-17 13:39:36 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Walls
-This is a simulation of bullets hitting walls to identify wall thickness.
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,40 @@
+var bullet, wall;
+var speed, weight, thickness;
+
+function setup() {
+  createCanvas(1600,400);
+  bullet = createSprite(50, 200, 40, 10);
+  speed = random(223,321);
+  weight = random(30,52);
+  thickness = random(22,83);
+  wall = createSprite(1200,200,thickness,200);
+  wall.shapeColor = color(80,80,80);
+}
+
+function draw() {
+  background("black");
+  bullet.velocityX = speed;
+  if(hasCollided(bullet, wall)) 
+  {
+    bullet.velocityX = 0;
+    var damage = 0.5 * weight * speed * speed / (thickness * thickness * thickness);
+    if (damage < 10) {
+      wall.shapeColor = color(0,255,0);
+    }
+    if (damage > 10) {
+      wall.shapeColor = color(255,0,0);
+    }
+  } 
+  drawSprites();
+}
+
+function hasCollided(lbullet, lwall)
+{
+  bulletRightEdge = lbullet.x + lbullet.width;
+  wallLeftEdge = lwall.x;
+  if (bulletRightEdge >= wallLeftEdge)
+  {
+    return true
+  }
+  return false;
+}
